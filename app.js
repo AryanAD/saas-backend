ec191761f5c2b18c7b358ffd2f80bc5fb7285361
AryanAD <aryanhohai@gmail.com>
2025-02-15 10:51:32 +0545

    [SETUP]: Middlewares

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 import { PORT as envPORT } from "./config/env.js";
 
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+
 import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+// Custom Middleware
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the SAAS server!");
