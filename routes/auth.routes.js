80f15d1751004842443f7c26250e50d9d9b32f8e
AryanAD <aryanhohai@gmail.com>
2025-02-15 11:27:11 +0545

    [SETUP]: Authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
+import { signIn, signOut, signUp } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-  res.send({ title: "User signed up successfully!" });
-});
-
-authRouter.post("/sign-in", (req, res) => {
-  res.send({ title: "User signed in successfully!" });
-});
-
-authRouter.post("/sign-out", (req, res) => {
-  res.send({ title: "User signed out successfully!" });
-});
+authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
